2eaf7c8b9926c84be22f928189dff6656a993ddb
yaffa7 <bmcmahon2@mail.csuchico.edu>
2019-01-06 11:39:42 -0800

    added css transisitons

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,37 @@ import data from './data/country.json'
 class App extends Component {
   constructor() {
     super()
-    this.state = data
+    this.state = {
+      data : data,
+      titleClassName: "title"
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        data: this.state.data,
+        titleClassName: "title show"
+      })
+    }, 200);
   }
   render() {
-    let cardData = this.state.data
+    let cardData = this.state.data.data
     return (
       <div>
+        <h2 className={this.state.titleClassName}>Quick Poll</h2>
+        <div className="center-container">
+          <div className="btn-center show">Create Poll</div>
+        </div>
         <div className="card-container">
           <div className="card-background">
-            <ResultsCard data={cardData[0]} key={cardData[0].question}/>
+            <ResultsCard data={cardData[0]} key={cardData[0].question} delay={800}/>
           </div>
           <div className="card-foreground">
-            <ResultsCard data={cardData[1]} key={cardData[1].question}/>
+            <ResultsCard data={cardData[1]} key={cardData[1].question} delay={500}/>
           </div>
           <div className="card-background">
-            <ResultsCard data={cardData[0]} key={cardData[0].question}/>
+            <ResultsCard data={cardData[0]} key={cardData[0].question} delay={800}/>
           </div>
         </div>
         {/* <CreateCard/> */}
--- src/components/results-card.jsx
+++ src/components/results-card.jsx
@@ -4,13 +4,23 @@ import './results-card.css'
 class ResultsCard extends Component {
   constructor(data) {
     super(data)
-    this.state = data
-    console.log(this.state)
+    this.state = {
+      cardData: data,
+      cardClassName: "card" ,
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        cardClassName: "card show"
+      })
+    }, this.props.delay);
   }
 
   totalVotes() {
     let sum = 0
-    this.state.data.options.forEach(op => {
+    this.state.cardData.data.options.forEach(op => {
       sum += op.value
     })
     return sum
@@ -18,45 +28,56 @@ class ResultsCard extends Component {
 
   winner() {
     let max = { value: 0 }
-    this.state.data.options.forEach(op => {
+    this.state.cardData.data.options.forEach(op => {
       if (op.value > max.value) {
         max = op
       }
     })
-
     return max.text
   }
 
   handleVote(answer) {
     let currState = this.state
-    currState.data.options.forEach(op => {
+    currState.cardData.data.options.forEach(op => {
       if (op.text === answer) {
         op.value++
       }
     })
-    this.setState({ data: currState.data })
+    this.setState({ 
+      cardData: currState.cardData,
+      cardClassName: this.state.cardClassName
+    })
+  }
+
+  getProgressWidthStyle(option){
+    let percentWidth = option.value/this.totalVotes() * 100
+    return {
+      width: `${percentWidth}%`
+    }
   }
 
   render() {
     return (
       <div>
-        <div className="card">
+        <div className={this.state.cardClassName}>
           <div className="header">
             <span className="votes">{this.totalVotes()}</span>
             {/* <img src={this.state.data.avatar_url} alt="" className="avatar" /> */}
             <span className="logo">Quick Poll</span>
           </div>
-          <h2>{this.state.data.question}</h2>
-          {this.state.data.options.map(option =>
+          <h2>{this.state.cardData.data.question}</h2>
+          {this.state.cardData.data.options.map(option =>
             <div onClick={() => this.handleVote(option.text)} className={this.winner() === option.text ? 'progress winner' : 'progress'} key={option.text}>
-              <span className="answer">{option.text}</span>
-              <span className="votes">{option.value}</span>
+            <div className={this.winner() === option.text ? 'progress-bar winner' : 'progress-bar'} key={option.text} style={this.getProgressWidthStyle(option)}></div>
+              <div className="level">
+                <span className="answer" style={this.winner() === option.text ? {color: '#636363'} : {color: '#b3b3b3'}}>{option.text}</span>
+                <span className="votes">{option.value}</span>
+              </div>
             </div>
           )}
         </div>
       </div>
     )
-
   }
 }
 
